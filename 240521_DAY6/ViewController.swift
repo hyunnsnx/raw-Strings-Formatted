0882d5bef31aefd46856f7070b9764f779d55052
binnyy <beanbaebae@naver.com>
2024-05-21 23:39:35 +0900

    Initial Commit

---
+++ 240521_DAY6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  240521_DAY6
+//
+//  Created by 배현빈 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
